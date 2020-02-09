cdce01fca5256fdc9a4fc731c5ba8f4f3afc9ded
Rotherford Marmibe <rmarmibe@gmail.com>
2020-02-10 00:19:00 +0800

    Moved weighted mean to the file mean

--- descriptive_statistics/mean.py
+++ descriptive_statistics/mean.py
@@ -1,14 +1,33 @@
-"""
-
-The mean or arithmitic average is operationally defined
-as the sum of scores divided by the number of scores.
-
-We use the mean when the greatest reliability is desired,
-when the distribution is normal, or not greatly skewed,
-and when there is a need for further statistical computation
-of other statistic like standard deviation and correlations
-"""
-
-
 def get_mean(items=[]):
+    """
+    The mean or arithmitic average is operationally defined
+    as the sum of scores divided by the number of scores.
+
+    We use the mean when the greatest reliability is desired,
+    when the distribution is normal, or not greatly skewed,
+    and when there is a need for further statistical computation
+    of other statistic like standard deviation and correlations
+    """
     return sum(items) / len(items)
+
+
+def get_weighted_mean(items=[{}]):
+    """
+    When scores are grouped we multiply first each
+    score x by its frequency f and divide the sum of 
+    products xf by total frequency N
+
+    weighted_mean = summation(x*f) / n
+
+    x = score
+    f = frequency
+    n = total frequency
+
+    """
+    total_freq = 0
+    total = 0
+    for item in items:
+        total = total + (item["score"] * item["freq"])
+        total_freq = total_freq + item["freq"]
+    print(total)
+    return total / total_freq
--- descriptive_statistics/mean_test.py
+++ descriptive_statistics/mean_test.py
@@ -1,6 +1,88 @@
-from mean import get_mean
+from mean import get_mean, get_weighted_mean
+
+
+def basic_mean_test():
+    assert get_mean([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 5.5
+    assert get_mean([1, 2, 3, 4, 5]) == 3.0
+
+
+def basic_weighted_mean_test():
+    assert (
+        get_weighted_mean(
+            [
+                {"score": 3, "freq": 4},
+                {"score": 4, "freq": 2},
+                {"score": 5, "freq": 5},
+                {"score": 6, "freq": 6},
+                {"score": 7, "freq": 3},
+            ]
+        )
+        == 5.1
+    )
+
+    assert (
+        round(
+            get_weighted_mean(
+                [
+                    {"score": 5, "freq": 12},
+                    {"score": 4, "freq": 16},
+                    {"score": 3, "freq": 2},
+                    {"score": 2, "freq": 0},
+                    {"score": 1, "freq": 0},
+                ]
+            ),
+            2,
+        )
+        == 4.33
+    )
+
+
+def four_machines_efficiency_test():
+    """
+    - Score refers to the rating from a evaluator of a machine
+    - Frequency refers to the number of times it was
+        given by an employee
+    """
+
+    machine_A_data = [
+        {"score": 5, "freq": 12},
+        {"score": 4, "freq": 16},
+        {"score": 3, "freq": 2},
+        {"score": 2, "freq": 0},
+        {"score": 1, "freq": 0},
+    ]
+
+    machine_B_data = [
+        {"score": 5, "freq": 17},
+        {"score": 4, "freq": 12},
+        {"score": 3, "freq": 1},
+        {"score": 2, "freq": 0},
+        {"score": 1, "freq": 0},
+    ]
+
+    machine_C_data = [
+        {"score": 5, "freq": 6},
+        {"score": 4, "freq": 7},
+        {"score": 3, "freq": 15},
+        {"score": 2, "freq": 2},
+        {"score": 1, "freq": 0},
+    ]
+
+    machine_D_data = [
+        {"score": 5, "freq": 6},
+        {"score": 4, "freq": 5},
+        {"score": 3, "freq": 12},
+        {"score": 2, "freq": 6},
+        {"score": 1, "freq": 1},
+    ]
+
+    assert round(get_weighted_mean(machine_A_data), 2) == 4.33
+    assert round(get_weighted_mean(machine_B_data), 2) == 4.53
+    assert round(get_weighted_mean(machine_C_data), 2) == 3.57
+    assert round(get_weighted_mean(machine_D_data), 2) == 3.30
 
 
 def test():
-    assert get_mean([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 5.5
-    assert get_mean([1, 2, 3, 4, 5]) == 3.0
+    basic_mean_test()
+    basic_weighted_mean_test()
+    four_machines_efficiency_test()
