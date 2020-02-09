97ac748c960e923fbd3a267e19a588a13ab84d71
Rotherford Marmibe <rmarmibe@gmail.com>
2020-02-10 02:02:17 +0800

    Added computation of median from frequency distribution with tests

--- descriptive_statistics/median.py
+++ descriptive_statistics/median.py
@@ -1,12 +1,5 @@
 from mean import get_mean
 
-"""
-The median is the point that divides the group into two
-equal parts - the upper half and the lower half.
-
-
-"""
-
 
 def is_even(num):
     if num % 2 == 0:
@@ -15,6 +8,10 @@ def is_even(num):
 
 
 def get_median(items=[]):
+    """
+    The median is the point that divides the group into two
+    equal parts - the upper half and the lower half.
+    """
     n = len(items)
     index = 0
     items.sort()
@@ -26,3 +23,54 @@ def get_median(items=[]):
     else:
         index = int(n + 1) / 2
         return items[int(index) - 1]
+
+def get_total_number_of_cases(items=[{}]):
+    total = 0
+    for item in items:
+        total = total + item['freq']
+    return total
+
+def create_cumulative_frequency(items=[{}]):
+    for index, item in enumerate(items):
+        if index != 0:
+            item["CF"] = item["freq"] + items[index - 1]["CF"]
+        else:
+            item["CF"] = item["freq"]
+        items[index] = item
+    return items
+
+
+def get_median_freq_dist(data=[{}], interval=0):
+    """
+    Formula for finding the median from a
+    Frequency Distribution:
+
+    median = lower_bound + (((number_of_cases/2) - cumulative_frequency) / frequency) *  interval
+    """
+    data = create_cumulative_frequency(data)
+    mean = get_mean(items=[data[0]["lower"], data[-1]["upper"]])
+    number_of_cases = get_total_number_of_cases(data)
+    rounded_mean = round(mean)
+    median_class = get_class_index(data, rounded_mean)
+    median = mean + (((number_of_cases/2) - 21) / 7) * interval
+    return median
+
+
+def get_class_index(items=[{}], mean=None):
+    for index, item in enumerate(items):
+        if mean >= item["lower"] or mean <= item["upper"]:
+            return index
+    return None
+
+
+def get_final_cumulative_frequency(items=[{}], number_of_cases=0, index=0):
+    boundary = round(number_of_cases / 2)
+    while(index != 0):
+        if  items[index]["CF"] < boundary and boundary >= items[index]["CF"]:
+            return index
+        else:
+            index = index - 1
+    return None
+
+
+
--- descriptive_statistics/median_test.py
+++ descriptive_statistics/median_test.py
@@ -1,6 +1,83 @@
-from median import get_median
+from median import get_median, get_median_freq_dist, create_cumulative_frequency, get_final_cumulative_frequency
+
+
+def test_median_freq_dist():
+    test_data = [
+        {"lower": 20, "upper": 24, "freq": 1},
+        {"lower": 25, "upper": 29, "freq": 2},
+        {"lower": 30, "upper": 34, "freq": 4},
+        {"lower": 35, "upper": 39, "freq": 3},
+        {"lower": 40, "upper": 44, "freq": 5},
+        {"lower": 45, "upper": 49, "freq": 6},
+        {"lower": 50, "upper": 54, "freq": 7},
+        {"lower": 55, "upper": 59, "freq": 5},
+        {"lower": 60, "upper": 64, "freq": 3},
+        {"lower": 65, "upper": 69, "freq": 4},
+        {"lower": 70, "upper": 74, "freq": 3},
+        {"lower": 75, "upper": 79, "freq": 2},
+    ]
+
+    assert round(get_median_freq_dist(data=test_data, interval=5), 2) == 50.57
+
+
+def test_cumulative_freq():
+    test_data = [
+        {"lower": 20, "upper": 24, "freq": 1},
+        {"lower": 25, "upper": 29, "freq": 2},
+        {"lower": 30, "upper": 34, "freq": 4},
+        {"lower": 35, "upper": 39, "freq": 3},
+        {"lower": 40, "upper": 44, "freq": 5},
+        {"lower": 45, "upper": 49, "freq": 6},
+        {"lower": 50, "upper": 54, "freq": 7},
+        {"lower": 55, "upper": 59, "freq": 5},
+        {"lower": 60, "upper": 64, "freq": 3},
+        {"lower": 65, "upper": 69, "freq": 4},
+        {"lower": 70, "upper": 74, "freq": 3},
+        {"lower": 75, "upper": 79, "freq": 2},
+    ]
+
+    expected_data = [
+        {"lower": 20, "upper": 24, "freq": 1, "CF": 1},
+        {"lower": 25, "upper": 29, "freq": 2, "CF": 3},
+        {"lower": 30, "upper": 34, "freq": 4, "CF": 7},
+        {"lower": 35, "upper": 39, "freq": 3, "CF": 10},
+        {"lower": 40, "upper": 44, "freq": 5, "CF": 15},
+        {"lower": 45, "upper": 49, "freq": 6, "CF": 21},
+        {"lower": 50, "upper": 54, "freq": 7, "CF": 28},
+        {"lower": 55, "upper": 59, "freq": 5, "CF": 33},
+        {"lower": 60, "upper": 64, "freq": 3, "CF": 36},
+        {"lower": 65, "upper": 69, "freq": 4, "CF": 40},
+        {"lower": 70, "upper": 74, "freq": 3, "CF": 43},
+        {"lower": 75, "upper": 79, "freq": 2, "CF": 45},
+    ]
+
+    assert create_cumulative_frequency(test_data) == expected_data
+
+def test_get_final_cumulative_frequency():
+    test_data = [
+        {"lower": 20, "upper": 24, "freq": 1, "CF": 1},
+        {"lower": 25, "upper": 29, "freq": 2, "CF": 3},
+        {"lower": 30, "upper": 34, "freq": 4, "CF": 7},
+        {"lower": 35, "upper": 39, "freq": 3, "CF": 10},
+        {"lower": 40, "upper": 44, "freq": 5, "CF": 15},
+        {"lower": 45, "upper": 49, "freq": 6, "CF": 21},
+        {"lower": 50, "upper": 54, "freq": 7, "CF": 28},
+        {"lower": 55, "upper": 59, "freq": 5, "CF": 33},
+        {"lower": 60, "upper": 64, "freq": 3, "CF": 36},
+        {"lower": 65, "upper": 69, "freq": 4, "CF": 40},
+        {"lower": 70, "upper": 74, "freq": 3, "CF": 43},
+        {"lower": 75, "upper": 79, "freq": 2, "CF": 45},
+    ]
+
+    assert get_final_cumulative_frequency(items=test_data, number_of_cases=45, index=6) == 5
+
+def basic_test():
+    assert get_median([5, 7, 4, 8, 15, 12, 2, 22, 18]) == 8
+    assert get_median([2, 3, 5, 7, 7, 9, 15, 12, 18, 22]) == 8
 
 
 def test():
-    assert get_median([5, 7, 4, 8, 15, 12, 2, 22, 18]) == 8
-    assert get_median([2, 3, 5, 7, 7, 9, 15, 12, 18, 22]) == 8
+    basic_test()
+    test_cumulative_freq()
+    test_get_final_cumulative_frequency()
+    test_median_freq_dist()
